27240acf4780d27a4915bebaecf27d66ff204eef
juhanajaa1 <juhana.jaakkola@gmail.com>
2026-03-06 14:46:56 +0200

    inputs

--- CPP_PROJECT_SPRING/renderer.cpp
+++ CPP_PROJECT_SPRING/renderer.cpp
@@ -1,6 +1,6 @@
 internal void
-clear_screen(unsigned int color) {
-	u32* pixel = (u32*)render_state.memory;
+clear_screen(u32 color) {
+	unsigned int* pixel = (u32*)render_state.memory;
 	for (int y = 0; y < render_state.height; y++) {
 		for (int x = 0; x < render_state.width; x++) {
 			*pixel++ = color;
@@ -24,12 +24,14 @@ draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color)
 	}
 }
 
+global_variable float render_scale = 0.01f;
+
 internal void
 draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
-	x *= render_state.height;
-	y *= render_state.height;
-	half_size_x *= render_state.height;
-	half_size_y *= render_state.height;
+	x *= render_state.height*render_scale;
+	y *= render_state.height*render_scale;
+	half_size_x *= render_state.height*render_scale;
+	half_size_y *= render_state.height*render_scale;
 
 	x += render_state.width / 2.f;
 	y += render_state.height / 2.f;
